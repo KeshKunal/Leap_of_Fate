ab3647d988b4032e493dec38c01784de998ab55d
KeshKunal <infini2ude@gmail.com>
2025-09-27 17:09:31 +0530

    direction sense

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
+const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add gravity.
@@ -15,8 +16,12 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Handle left/right movement.
+	# Gets input direction -1/0/1
 	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction > 0:
+		animated_sprite.flip_h = flase
+	if direction <0:
+		animated_sprite.flip_h = true
 	if direction:
 		velocity.x = direction * SPEED
 	else:
